1234ac874967861b5a5a16f114f4d53524bce179
barismar <barisma99@gmail.com>
2022-04-09 08:50:16 +0700

    add singly linked list

---
+++ algorithms-and-data-structures/linkedlist/main.go
@@ -0,0 +1,42 @@
+package main
+
+import "fmt"
+
+type Node struct {
+	data int
+	next *Node
+}
+
+type LinkedList struct {
+	head   *Node
+	length int
+}
+
+func (ll *LinkedList) Insert(data int) {
+	newNode := Node{data: data, next: nil}
+	if ll.head == nil {
+		ll.length++
+		ll.head = &newNode
+	} else {
+		current := ll.head
+		for current.next != nil {
+			current = current.next
+		}
+		current.next = &newNode
+	}
+}
+
+func (ll *LinkedList) Display() {
+	current := ll.head
+	for current != nil {
+		fmt.Println(current.data)
+		current = current.next
+	}
+}
+
+func main() {
+	linkedList := LinkedList{}
+	linkedList.Insert(1)
+	linkedList.Insert(2)
+	linkedList.Display()
+}
